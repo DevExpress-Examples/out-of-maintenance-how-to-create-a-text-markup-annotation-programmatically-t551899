51199041ebf0715dbc86b082e18f844281f29cba
DevExpressExampleBot <fakeEmail>
2018-06-09 16:45:14 +0300

    Source auto update [skip ci]

---
+++ VB/CreateMarkupAnnotation/Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Drawing
+Imports System.Windows.Forms
+
+Namespace CreateMarkupAnnotation
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+
+            ' Load a document.
+            pdfViewer1.LoadDocument("..\..\Demo.pdf")
+
+            ' Select the document area where the markup annotation will be created.
+            pdfViewer1.Select(New RectangleF(-230, 420, 1240, 150))
+
+            ' Highlight a selected text.
+            pdfViewer1.HighlightSelectedText()
+        End Sub
+    End Class
+End Namespace
