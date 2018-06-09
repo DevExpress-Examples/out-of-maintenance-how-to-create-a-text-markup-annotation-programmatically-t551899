71541357675550c8f2a535936b3d5be5d81ef4b4
Alexander Temnov (DevExpress) <temnov.alexander@corp.devexpress.com>
2018-06-09 12:31:02 +0300

    upfate example

--- CS/CreateMarkupAnnotation/Properties/AssemblyInfo.cs
+++ CS/CreateMarkupAnnotation/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApplication4")]
+[assembly: AssemblyTitle("CreateMarkupAnnotation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApplication4")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("CreateMarkupAnnotation")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9a5f3b28-64da-4ecc-923e-8cfbdc28bc7f")]
+[assembly: Guid("e1306b35-47c6-4a5f-b4fa-abfed2079b87")]
 
 // Version information for an assembly consists of the following four values:
 //
